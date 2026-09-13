4cbc996f251fe0ca801b2d9ad6c5fa06a44f8d0c
onesolutions-ahuja <tanha.ahuja2@gmail.com>
2026-09-13 20:36:08 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ from app.routes.suppliers import router as suppliers_router
 from app.routes.purchase_orders import router as purchase_orders_router
 from app.routes.product_suppliers import router as product_suppliers_router
 from app.routes.sales import router as sales_router
+from app.routes.customers import router as customers_router
 
 app = FastAPI(
     title="OneStoreSolution",
@@ -29,6 +30,7 @@ app.include_router(suppliers_router)
 app.include_router(purchase_orders_router)
 app.include_router(product_suppliers_router)
 app.include_router(sales_router)
+app.include_router(customers_router)
 
 @app.get("/")
 def root():
