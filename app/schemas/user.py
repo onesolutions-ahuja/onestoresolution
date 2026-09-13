a673b37aab9916575a12ed5701b54d68c94d0734
onesolutions-ahuja <tanha.ahuja2@gmail.com>
2026-09-13 16:40:49 +0100

    Create user.py

---
+++ app/schemas/user.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    role: str
+
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    role: str
+    is_active: bool
