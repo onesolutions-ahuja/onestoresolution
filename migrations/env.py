961697f4e87f1f068371aba554bf3e2502351461
onesolutions-ahuja <tanha.ahuja2@gmail.com>
2026-09-13 15:42:40 +0100

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,4 +1,17 @@
+import os
+import sys
 from logging.config import fileConfig
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+from app.database import Base
+from app.models.user import User
+from app.models.store import Store
+from app.models.role import Role
+from app.models.user_store_access import UserStoreAccess
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
