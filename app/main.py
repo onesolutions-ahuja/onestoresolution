e63dd41e15b65a89fe4dcdf4bbdc83b4308457a0
onesolutions-ahuja <tanha.ahuja2@gmail.com>
2026-09-13 16:41:58 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.database import engine
 from app.routes.auth import router as auth_router
 from app.routes.stores import router as stores_router
 from app.routes.store_access import router as store_access_router
+from app.routes.users import router as users_router
 
 app = FastAPI(
     title="OneStoreSolution",
@@ -15,6 +16,7 @@ app = FastAPI(
 app.include_router(auth_router)
 app.include_router(stores_router)
 app.include_router(store_access_router)
+app.include_router(users_router)
 
 
 @app.get("/")
